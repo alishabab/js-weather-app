aa786be9186fe037a78f8dfcf08db0dcb97eb7bf
Shabab <shababsaifi@gmail.com>
2020-08-06 20:25:16 +0530

    Added method to get icon based on weather condition

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.scss';
 
-const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const appId = 'a6fffd615dee8a18a1d160f3eead85a3';
 
 
@@ -8,29 +9,31 @@ const getBtn = document.querySelector('#getBtn')
 
 const weatherDiv = document.querySelector('#weatherDiv')
 
-const getWeatherData = async (e) => {
-  const city = document.querySelector('#city').value
-  fetch(`${baseUrl}${city}&appid=${appId}`)
-    .then(response => {
-      if(response.ok) {
-        return response.json()
-      } else {
-        throw new Error("City Not Found!")
-      }
-    })
-    .then(response => {
-      renderData(response)
-      console.log(response)
-    })
-    .catch(err => {
-      console.log(err)
-    })
+const getData = async (url) => {
+ const response = await fetch(url)
+ if (response.ok) {
+   return response.json()
+ } else {
+   throw new Error('Something went wrong!!')
+ }
 }
 
-const renderData = (data) => {
+const getWeatherData = async () => {
+  const city = document.querySelector('#city').value
+  const weatherData = await getData(`${baseUrl}?q=${city}&appid=${appId}`)
+  const iconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+  console.log(weatherData) 
+  renderData(weatherData, iconUrl)
+}
+
+
+const renderData = (weatherData, iconUrl) => {
   clearElement(weatherDiv);
   const h1 = document.createElement('h1')
-  h1.textContent = data.name
+  const img = document.createElement('img')
+  img.src = iconUrl
+  h1.textContent = weatherData.name
+  weatherDiv.appendChild(img)
   weatherDiv.appendChild(h1)
 }
 
